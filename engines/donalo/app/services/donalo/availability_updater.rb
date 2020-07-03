cecba1650accd0bfbc346b66822292d7968ed948
Jorge Morante <jorge@morante.eu>
2020-07-03 18:00:49 +0200

    update availability right after updating stock

--- engines/donalo/app/services/donalo/availability_updater.rb
+++ engines/donalo/app/services/donalo/availability_updater.rb
@@ -1,19 +1,9 @@
 module Donalo
   class AvailabilityUpdater < StockUpdater
     def update
-      return unless should_be_updated?
-
-      listing.open = false
+      listing.open = current_quantity > 0
       listing.save
     end
-
-    private
-
-    def should_be_updated?
-      listing && stock && current_quantity <= 0
-    end
-
-    attr_reader :transaction_id
   end
 end
 
